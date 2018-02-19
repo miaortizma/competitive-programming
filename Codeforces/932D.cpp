bbac337c28fa0eccf4468d62d8a29ad2aec008c2
miaortizma <miaortizma@unal.edu.co>
2018-02-19 15:01:35 -0500

    Update 932D.cpp

--- Codeforces/932D.cpp
+++ Codeforces/932D.cpp
@@ -2,30 +2,19 @@
 #define ll long long
 using namespace std;
 
+void print(vector<ll> v){
+    for(int i = 0; i < v.size(); i++) cout << v[i] << ' ';
+    cout << '\n';
+}
+
 ll solve(vector<ll> v, ll W){
     int n = v.size();
-    vector<ll>::iterator low = lower_bound(v.begin(), v.end(), W);
-    /*cout << "Weights\n";
-    for(int i = 0; i < v.size(); i++){
-        cout << v[i] << ' ';
-    }
-    cout << "\nind";
-    cout << *low << ' ' << low - v.begin() + 1;*/
-    int pos = low - v.begin();
-    if(pos == n) return n;
-    if(*low > W){
-        //cout << "<";
-        if(low == v.begin()){
-            return 0;
-        }else{
-            low--;
-            return low - v.begin() + 1;
-        }
-    }else{
-        //cout << "<<";
-        return low - v.begin() + 1;
-    }
-    return 1;
+    vector<ll>::iterator up = upper_bound(v.begin(), v.end(), W);
+    //print(v);
+    //cout <<"up:" <<*up<<'\n';
+    if(up == v.begin()) return 0;
+    if(up == v.end()) return n;
+    return up - v.begin();
 }
 
 int main(){
@@ -34,53 +23,48 @@ int main(){
     ll last = 0,t, p, q;
     vector< vector<ll> >node, weights;
     vector<ll> v0;
-    // weight, index
     v0.push_back(0);
     weights.push_back(v0);
     node.push_back(v0);
     while(Q--){
+        //cout <<"\nQ::"<<  Q << '\n';
         cin >> t >> p >> q;
         p = p^last;
         q = q^last;
-        //cout << p << ' ' << q << '\n';
+        //cout << "p\tq\n";
         p--;
+        //cout << p << '\t' << q << '\n';
         if(t == 1){
             vector<ll> v = node[p];
-            vector<ll>::iterator low = v.begin();
             vector<ll> next, next1;
             ll w = q;
-            /*cout << "Parent\n";
-            for(int i = 0; i < v.size(); i++){
-                cout << v[i] << ' ';
-            }cout << '\n';*/
+            vector<ll>::iterator low = lower_bound(v.begin(), v.end(), w);
+            //cout <<"low: " <<  *low << '\n';
+            //cout <<"q: " << w << '\n';
             next.push_back(q);
             next1.push_back(w);
             while(low != v.end()){
-                low = lower_bound(v.begin(), v.end(), q);
-                //cout << *low << '\n';
-                if(low == v.end()) break;
                 q = *low;
-                w+=q;
+                w += q;
+                low++;
                 next.push_back(q);
                 next1.push_back(w);
-                low++;
             }
             node.push_back(next);
             weights.push_back(next1);
-            /*cout << "\n----\n";
-            for(int i = 0; i < next.size(); i++){
-                cout << next[i] << ' ';
-            }
-            cout << '\n';
-            for(int i = 0; i < next1.size(); i++){
-                cout << next1[i] << ' ';
-            }
-            cout << "Good\n";*/
+
+            int n = node[node.size() - 1].size();
+            int a = node.size();
+            //cout << "Node values:\n";
+            //print(node[a-1]);
+            //cout << "\nWeight values added:\n";
+            //print(weights[a-1]);
+
         }else{
             last = solve(weights[p], q);
+            //cout << "last:\n";
             cout << last << '\n';
         }
-        //cout << '\n';
     }
     return 0;
 }
